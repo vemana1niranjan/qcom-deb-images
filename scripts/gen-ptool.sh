5637018a2032ddbeb5e59896c5f90d6f78b2e432
Loïc Minier <loic.minier@oss.qualcomm.com>
2026-04-08 13:11:22 +0000

    refactor(debos/flash): Infer storage from platform
    Avoid parsing partitions.conf as this is error prone and an internal
    ptool format that might change in the future (e.g. to support %include
    directives). Instead, infer disk storage type from platform name.
    
    Signed-off-by: Loïc Minier <loic.minier@oss.qualcomm.com>

--- scripts/gen-ptool.sh
+++ scripts/gen-ptool.sh
@@ -17,61 +17,30 @@ PLATFORM="$2"
 CDT_FILENAME="$3"
 # build id for generated contents.xml
 BUILDID="$4"
+# disk storage, emmc, nvme, spinor or ufs
+DISK_TYPE="$5"
 
-# path to partitions.conf
 PARTITIONS_CONF="${QCOM_PTOOL}/platforms/${PLATFORM}/partitions.conf"
 
-disk_type="unknown"
-
-# make a copy of partitions.conf; infer storage type from lines
-# like:
-#     --disk --type=ufs --size=137438953472 ...
-# or:
-#     --disk --type=emmc --size=76841669632 ...
-# and patch/add filenames for partitions from lines like:
-#     --partition --lun=4 --name=dtb_a ... --filename=dtb.bin
-# or without filename like:
-#     --partition --lun=3 --name=cdt --size=128KB --type-guid=...
-# for data from the following table
-#
-# |--------|--------------|-------------------|-----------------|
-# | data   | ptool name   | ptool filename    | debos filename  |
-# |--------|--------------|-------------------|-----------------|
-# | ESP    | efi          | efi.bin           | disk-media.img1 |
-# | rootfs | rootfs       | rootfs.img        | disk-media.img2 |
-# | DTBs   | dtb_a, dtb_b | dtb.bin           | dtb.bin         |
-# | CDTs   | cdt          | unset / per board | from download   |
-# |--------|--------------|-------------------|-----------------|
-
-while read -r line; do
-    case "$line" in
-        # detect storage type
-        "--disk "*)
-            disk_type="$(echo "$line" | sed -n 's/.*--type=\([^ ]*\).*/\1/p')"
-            case $disk_type in
-                emmc|nvme)
-                    esp="../disk-sdcard.img1"
-                    rootfs="../disk-sdcard.img2"
-                    ;;
-                ufs)
-                    esp="../disk-ufs.img1"
-                    rootfs="../disk-ufs.img2"
-                    ;;
-                spinor)
-                    # spinor carries firmware only; no OS efi/rootfs partitions
-                    esp=""
-                    rootfs=""
-                    ;;
-                *)
-                    echo "unsupported disk type $disk_type"
-                    exit 1
-                    ;;
-                esac
-            echo "$disk_type" >disk_type
-        ;;
-    esac
-    echo "$line"
-done <"${PARTITIONS_CONF}"
+case "$DISK_TYPE" in
+  emmc|nvme)
+    esp="../disk-sdcard.img1"
+    rootfs="../disk-sdcard.img2"
+    ;;
+  ufs)
+    esp="../disk-ufs.img1"
+    rootfs="../disk-ufs.img2"
+    ;;
+  spinor)
+    # spinor carries firmware only; no OS efi/rootfs partitions
+    esp=""
+    rootfs=""
+    ;;
+  *)
+    echo "unsupported disk type $DISK_TYPE"
+    exit 1
+    ;;
+esac
 
 # build a map of partition names from partitions.conf to our names
 #
